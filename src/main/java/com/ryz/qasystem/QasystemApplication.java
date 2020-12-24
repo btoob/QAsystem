54e3a15ee743c976a3f42d024d1bd7acefd50393
btoob <ryz311@mail.ustc.edu.cn>
2020-12-24 15:43:26 +0800

    init repo

---
+++ src/main/java/com/ryz/qasystem/QasystemApplication.java
@@ -0,0 +1,13 @@
+package com.ryz.qasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QasystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QasystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryz/qasystem/QasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryz.qasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QasystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
