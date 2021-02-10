1a9d1c2817d65b6106c9c61808bdc3a9aca14d78
btoob <ryz311@mail.ustc.edu.cn>
2021-02-11 00:10:59 +0800

    add springsecurity

---
+++ src/main/java/com/ryz/qasystem/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.ryz.qasystem.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ryz.qasystem.model.RespBean;
+import com.ryz.qasystem.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.io.PrintWriter;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginPage("/login")
+                .loginProcessingUrl("/dologin")
+                .successHandler((req, resp, authentication)->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    User user = (User) authentication.getPrincipal();
+                    user.setPassword(null);
+                    RespBean ok = RespBean.ok("登录成功", user);
+                    String s = new ObjectMapper().writeValueAsString(ok);   //将这个user写成一个字符串
+
+                    out.write(s);
+                    out.flush();
+                    out.close();
+
+                })
+                .failureHandler((res, resp, e)->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+
+                    RespBean error = RespBean.error("登录失败");
+                    //判断失败的原因
+                    if(e instanceof LockedException){
+                        error.setMsg("账户被锁定，请联系管理员");
+                    }else if(e instanceof CredentialsExpiredException){
+                        error.setMsg("密码过期，请联系管理员");
+                    }else if(e instanceof AccountExpiredException){
+                        error.setMsg("账户过期，请联系管理员");
+                    }else if(e instanceof DisabledException){
+                        error.setMsg("账户被禁用，请联系管理员");
+                    }else if(e instanceof BadCredentialsException){
+                        error.setMsg("用户名或者密码错误，请重新输入");
+                    }
+
+                    String s = new ObjectMapper().writeValueAsString(error);
+
+                    out.write(s);
+                    out.flush();
+                    out.close();
+
+                })
+                .permitAll()   //表示登录相关的页面或者接口不要被拦截
+                .and()
+                .csrf().disable();
+    }
+}
