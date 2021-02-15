c05d7e8f01f1532c1623f108ef8eb4bbca2f7b83
btoob <ryz311@mail.ustc.edu.cn>
2021-02-15 18:23:43 +0800

    fix bug

--- src/main/java/com/ryz/qasystem/controller/UserController.java
+++ src/main/java/com/ryz/qasystem/controller/UserController.java
@@ -11,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @RestController
 public class UserController {
